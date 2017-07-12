1a2ef25e55470c6fbaab27685ea8be296609fe8e
Anastasiia <anask95@gmail.com>
2017-07-12 15:17:46 +0300

    Added structure of test

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ class TestSuite(unittest.TestCase):
             r"c:\Users\an.kravets\Downloads\PDFRedirect\PDFRedirect.exe")
         self.main_window = self.app.PDFRedirect
 
+    #TODO screenshot for fail
     def tearDown(self):
         print(self._testMethodName)
         while True:
@@ -19,24 +20,62 @@ class TestSuite(unittest.TestCase):
             else:
                 break
 
-    def test_default_view_first(self):
-        utilities.default_view(self.main_window)
 
     @unittest.skip('pass')
-    def test_1_first_launch(self):
+    def test_0_default_view_first(self):
+        utilities.default_view(self.main_window)
+
+    #@unittest.skip('pass')
+    def test_set_pdf_reader(self):
+        utilities.set_default_reader(self.main_window)
+
+    #TODO
+    @unittest.skip('pass')
+    def test_set_printers_settings(self):
+        pass
+
+    #TODO
+    @unittest.skip('pass')
+    def test_matching_paper_and_printer_settings(self):
+        pass
+
+    @unittest.skip('pass')
+    def test_apply_setting_without_pdf_reader(self):
+        pass
+
+    @unittest.skip('pass')
+    def test_apply_settings_without_text_pattern(self):
+        pass
+
+    @unittest.skip('pass')
+    def test_first_launch(self):
         #self.main_window.print_control_identifiers()
         utilities.set_default_reader(self.main_window)
         utilities.input_text_pattern(self.main_window, 'MetaData')
+        utilities.apply_settings(self.main_window)
         # time.sleep(2)
+
+    #TODO
+    @unittest.skip('pass')
+    def test_open_saved_setting(self):
+        pass
 
     @unittest.skip('pass')
     def test_send_an_error_report(self):
-        utilities.send_an_error_report(self.app, 'Description error message')
-        time.sleep(1)
+        utilities.type_an_error_report(self.app, 'Test ')
+        #self.app['Description Error'].Send.click()
+        utilities.send_error_confirmation_message(self.app)
 
     @unittest.skip('pass')
-    def test_set_printers_settings(self):
-        utilities.change_dropdown_list(self.main_window)
+    def test_send_an_empty_error_report(self):
+        utilities.type_an_error_report(self.app)
+        #self.app['Description Error'].Send.click()
+        utilities.send_error_confirmation_message()
+
+    # TODO scroll test
+    @unittest.skip('pass')
+    def test_scrolling_error_report(self):
+        utilities.type_an_error_report(self.app, 'Test '*120)
 
     @unittest.skip('pass')
     def test_close_ALT_F4(self):
--- utilities.py
+++ utilities.py
@@ -1,13 +1,16 @@
 import time
 
-def default_view(window, name_edit = None, text_edit = None, \
-                 printer_name_list ='Microsoft XPS Document Writer', \
-                 paper_format_list = 'Letter', \
-                 paper_source_list = 'Автовыбор', \
-                 duplex_list = 'Simplex'):
+
+def default_view(window, name_edit='',
+                 text_edit='',
+                 printer_name_list='Microsoft XPS Document Writer',
+                 paper_format_list='Letter',
+                 paper_source_list='Автовыбор',
+                 duplex_list='Simplex'):
     settings_static = 'Settings'
     default_pdf_program_static = 'Default PDF program'
     name_static = 'Name:'
+    open_button_text = '...'
     text_pattern_static = 'Text pattern'
     text_static = 'Text:'
     printer_settings_static = 'Printer settings'
@@ -16,14 +19,20 @@ def default_view(window, name_edit = None, text_edit = None, \
     paper_source_static = 'Paper source:'
     duplex_static = 'Duplex:'
     first_element = 0
+    send_error_button = window.Button
+    send_error_button_text = 'Send an error report'
+    apply_button = window.Button2
+    apply_button_text = 'Apply'
+    open_button = window.Button3
 
     assert window.Settings.texts()[first_element] in settings_static
     assert window['Default PDF program'].texts()[first_element] in default_pdf_program_static
     assert window['Name:Static'].texts()[first_element] in name_static
-    #assert window['Name:Edit'].texts()[first_element] == name_edit
+    assert window['Name:Edit'].texts()[first_element] == name_edit
+    assert open_button.texts()[first_element] in open_button_text
     assert window['Text pattern'].texts()[first_element] in text_pattern_static
     assert window['Text:Static'].texts()[first_element] in text_static
-    #assert window['Text:Edit'].texts()[first_element] == text_edit
+    assert window['Text:Edit'].texts()[first_element] == text_edit
     assert window['Printer settings'].texts()[first_element] in printer_settings_static
     assert window['Printer name:Static'].texts()[first_element] in printer_name_static
     assert window['Printer name:ComboBox'].SelectedText() in printer_name_list
@@ -33,13 +42,24 @@ def default_view(window, name_edit = None, text_edit = None, \
     assert window['Paper source:ComboBox'].SelectedText() in paper_source_list
     assert window['Duplex:Static'].texts()[first_element] in duplex_static
     assert window['Duplex:ComboBox'].SelectedText() in duplex_list
-    window.Button.verify_actionable()   # is Send an error report button active
-    window.Button2.verify_visible()     # is Apply button visible
-    window.Button3.verify_actionable()  # is ...Button active
-    print(window.Button.texts())
+    assert apply_button.texts()[first_element] in apply_button_text
+    assert send_error_button.texts()[first_element] in send_error_button_text
+    send_error_button.verify_actionable()
+    apply_button.verify_visible()
+    open_button.verify_actionable()
 
 
+def description_error_view(window):
+    descr_err_window = window['Description Error']
+    send_button = descr_err_window.Button
+    text_static = 'Text'
+    text_edit = ''
+    send_button_text = 'Send'
 
+    assert descr_err_window.TextStatic.texts() in text_static
+    assert descr_err_window.Edit.texts() in text_edit
+    assert descr_err_window.Button.texts() in send_button_text
+    send_button.verify_actionable()
 
 
 def set_default_reader(window):
@@ -47,14 +67,14 @@ def set_default_reader(window):
     # TODO opening file
 
 
-def input_text_pattern(window, text = ''):
+def input_text_pattern(window, text=''):
     text_field = window.edit
     text_field.type_keys(text, with_spaces=True)
     index = 0
     assert text_field.texts()[index] in text
 
 
-def input_text_pattern_CTRL_C(window, text = ''):
+def input_text_pattern_ctrl_c(window, text=''):
     text_field = window.edit
     text_field.type_keys(text, with_spaces=True)
     text_field.type_keys('^(a)^(c)')
@@ -65,16 +85,25 @@ def input_text_pattern_CTRL_C(window, text = ''):
     assert text_field.texts()[index] in text
 
 
-def applying_settings(window):
+# TODO raise exception if the program still runing
+def apply_settings(window):
     window.Apply.wait('active').click()
+    if window.is_process_running()is False:
+        print('Something wrong')
 
 
-def send_an_error_report(window, error_text = ''):
+def type_an_error_report(window, error_text=''):
     window.PDFRedirect.Button.click()
     descr_error_window = window['Description Error']
     descr_error_window.edit.type_keys(error_text, with_spaces=True)
-    # descr_error_window.Send.click()
-    #TODO asserting confirmation message
+
+
+# TODO asserting confirmation message
+def send_error_confirmation_message(window):
+    confirm_msg = window.top_window()
+    first_element = 0
+    assert confirm_msg.Static.texts()[first_element] in 'Message sent'
+    assert confirm_msg.Button.texts()[first_element] in 'OK'
 
 
 def change_dropdown_list(window):
