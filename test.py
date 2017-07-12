85987ac8f7565ebf7b2055fa439ea17b8b4ecb08
Anastasiia <anask95@gmail.com>
2017-07-12 10:45:44 +0300

    Added first default view test

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from pywinauto.application import Application
 import time
 import utilities
 
+
 class TestSuite(unittest.TestCase):
 
     def setUp(self):
@@ -18,23 +19,28 @@ class TestSuite(unittest.TestCase):
             else:
                 break
 
-    #@unittest.skip('pass')
+    def test_default_view_first(self):
+        utilities.default_view(self.main_window)
+
+    @unittest.skip('pass')
     def test_1_first_launch(self):
         #self.main_window.print_control_identifiers()
         utilities.set_default_reader(self.main_window)
         utilities.input_text_pattern(self.main_window, 'MetaData')
-        #time.sleep(2)
+        # time.sleep(2)
 
-    #@unittest.skip('pass')
+    @unittest.skip('pass')
     def test_send_an_error_report(self):
         utilities.send_an_error_report(self.app, 'Description error message')
         time.sleep(1)
 
+    @unittest.skip('pass')
     def test_set_printers_settings(self):
         utilities.change_dropdown_list(self.main_window)
 
-    def test_0close_ALT_F4(self):
-        self.main_window.edit.type_keys('%({F4})')
+    @unittest.skip('pass')
+    def test_close_ALT_F4(self):
+        self.main_window.close_alt_f4()
         assert self.app.is_process_running() is False
 
 
